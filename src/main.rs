4982cad900692fcb56aa910fde072ec9f078528e
Kailokk <kailokk.converse@gmail.com>
2024-09-12 23:57:35 +0100

    Basic output of current workspace state

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use swayipc::{Connection, EventType, Fallible};
+
+fn main() -> Fallible<()> {
+    let subs = [
+        EventType::Workspace,
+        EventType::Shutdown,
+        EventType::Mode,
+        EventType::Output,
+        ];
+    for _event in Connection::new()?.subscribe(subs)? {
+        Connection::new()?.get_workspaces()?.iter().for_each(|workspace|{
+            println!("Workspace found \'{}\', with id \'{}\', on output \'{}\', is visible: {}",workspace.name,workspace.id,workspace.output,workspace.visible);
+        });
+        println!("----------------------------------------------------------------------")
+    }
+    Ok(())
 }
