b2c661a932f1cba28d498ed4c572f6ca58c3624b
Kailokk <kailokk.converse@gmail.com>
2024-09-13 00:44:59 +0100

    Basic output with serde

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use serde::Serialize;
 use swayipc::{Connection, EventType, Fallible};
 
 fn main() -> Fallible<()> {
@@ -7,11 +8,29 @@ fn main() -> Fallible<()> {
         EventType::Mode,
         EventType::Output,
         ];
+
     for _event in Connection::new()?.subscribe(subs)? {
-        Connection::new()?.get_workspaces()?.iter().for_each(|workspace|{
-            println!("Workspace found \'{}\', with id \'{}\', on output \'{}\', is visible: {}",workspace.name,workspace.id,workspace.output,workspace.visible);
-        });
-        println!("----------------------------------------------------------------------")
+       let workspaces:Box<[Workspace]> = Connection::new()?.get_workspaces()?.iter().map(|workspace|{
+            
+           Workspace{
+                name: Box::from(workspace.name.as_ref()),
+                output: Box::from(workspace.output.as_ref()),
+                visible: workspace.visible,
+                focused: workspace.focused,
+                urgent: workspace.urgent,
+            }}
+        )
+        .collect();
+        println!("{}", serde_json::to_string(&workspaces).expect("Failed to serialize workspaces"));
     }
     Ok(())
+}
+
+#[derive(Serialize)]
+struct Workspace{
+    name:Box<str>,
+    output:Box<str>,
+    visible:bool,
+    urgent:bool,
+    focused:bool,
 }
