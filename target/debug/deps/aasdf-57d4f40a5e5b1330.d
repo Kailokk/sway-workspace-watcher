ba170bacfcd277bb599a45a9ec39303fb5d83619
Kailokk <kailokk.converse@gmail.com>
2024-09-13 02:19:56 +0100

    Remove target directory

--- target/debug/deps/aasdf-57d4f40a5e5b1330.d
+++
@@ -1,5 +0,0 @@
-/home/kailokk/repos/swaywatchrs/aasdf/target/debug/deps/libaasdf-57d4f40a5e5b1330.rmeta: src/main.rs
-
-/home/kailokk/repos/swaywatchrs/aasdf/target/debug/deps/aasdf-57d4f40a5e5b1330.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/aasdf-7ee4baf418f9687e.d
+++
@@ -1,5 +0,0 @@
-/home/kailokk/repos/swaywatchrs/aasdf/target/debug/deps/libaasdf-7ee4baf418f9687e.rmeta: src/main.rs
-
-/home/kailokk/repos/swaywatchrs/aasdf/target/debug/deps/aasdf-7ee4baf418f9687e.d: src/main.rs
-
-src/main.rs:
