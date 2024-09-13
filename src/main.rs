ffbeffff5b81cc3a7ade9f31f26b38231bd93063
Kailokk <kailokk.converse@gmail.com>
2024-09-13 02:49:17 +0100

    Use a validated json format

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,7 @@ fn main() {
             })
             .collect();
         println!(
-            "{{{}}}",
+            "{{\"workspaces\":{}}}",
             serde_json::to_string(&workspaces).expect("Failed to serialize workspaces")
         );
     });
